48b0426684c8a26d0b9a70096ef82558d5442bbe
hafezfhmi <hafezfahmi2000@gmail.com>
2022-07-08 00:25:43 +0800

    Tutorial 2/8 = Circle animation when circle render, and change circle size on click

--- src/App.js
+++ src/App.js
@@ -1,45 +1,88 @@
-import { useRef, useEffect } from "react";
-import logo from "./logo.svg";
+import { useRef, useEffect, useState } from "react";
 import "./App.css";
 
 import { gsap, Power3 } from "gsap";
 
 function App() {
-  let logoItem = useRef(null);
-  let textItem = useRef(null);
+  let app = useRef(null);
+  let circle = useRef(null);
+  let circleRed = useRef(null);
+  let circleBlue = useRef(null);
+
+  const [state, setState] = useState(false);
+
+  /* Animate circleRed onClick */
+  const handleExpand = () => {
+    gsap.to(circleRed.current, {
+      duration: 0.8,
+      width: 200,
+      height: 200,
+      ease: Power3.easeOut,
+    });
+
+    setState(true);
+  };
+
+  /* Animate circleRed onClick */
+  const handleShrink = () => {
+    gsap.to(circleRed.current, {
+      duration: 0.8,
+      width: 75,
+      height: 75,
+      ease: Power3.easeOut,
+    });
+
+    setState(false);
+  };
 
   useEffect(() => {
-    console.log(logoItem.current);
+    /* Make app component visible */
+    gsap.to(app.current, { duration: 0, css: { visibility: "visible" } });
 
-    gsap.to(logoItem.current, 0.8, {
-      opacity: 1,
-      y: -20,
+    /* Make the circles appear with animation */
+    gsap.from([circle.current, circleRed.current, circleBlue.current], {
+      opacity: 0,
+      x: 40,
       ease: Power3.easeOut,
+      duration: 0.8,
+      stagger: 0.2,
     });
 
-    gsap.to(textItem.current, 0.8, {
-      opacity: 1,
-      y: -20,
-      ease: Power3.easeOut,
-      delay: 0.2,
-    });
-  }, [logoItem, textItem]);
+    /* Make the circles appear with animation */
+    // gsap.from(circle.current, {
+    // duration: 0.8,
+    // opacity: 0,
+    // x: 40,
+    // ease: Power3.easeOut,
+    // });
+    // gsap.from(circleRed.current, {
+    //   duration: 0.8,
+    //   opacity: 0,
+    //   x: 40,
+    //   ease: Power3.easeOut,
+    //   delay: 0.2,
+    // });
+    // gsap.from(circleBlue.current, {
+    //   duration: 0.8,
+    //   opacity: 0,
+    //   x: 40,
+    //   ease: Power3.easeOut,
+    //   delay: 0.4,
+    // });
+  }, []);
 
   return (
-    <div className="App">
+    <div className="App" ref={app}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" ref={logoItem} />
-        <p ref={textItem}>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="circle-container">
+          <div className="circle" ref={circle}></div>
+          <div
+            className="circle red"
+            ref={circleRed}
+            onClick={state !== true ? handleExpand : handleShrink}
+          ></div>
+          <div className="circle blue" ref={circleBlue}></div>
+        </div>
       </header>
     </div>
   );
