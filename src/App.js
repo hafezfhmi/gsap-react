731d955ee91e84172fdc1414ceb306df060f340b
hafezfhmi <hafezfahmi2000@gmail.com>
2022-07-07 23:25:08 +0800

    Tutorial 1/8 = Setup + using gsap.to to make simple animation on React ref object

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { gsap, Power3 } from "gsap";
 
 function App() {
+  let logoItem = useRef(null);
+
+  useEffect(() => {
+    console.log(logoItem.current);
+
+    gsap.to(logoItem.current, 0.8, {
+      opacity: 1,
+      y: -20,
+      ease: Power3.easeOut,
+    });
+  }, [logoItem]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" ref={logoItem} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
