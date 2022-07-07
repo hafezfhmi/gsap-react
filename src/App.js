a359f8e9ea16ca52945f1a146fa23792d7be1968
hafezfhmi <hafezfahmi2000@gmail.com>
2022-07-08 01:20:20 +0800

    Tutorial 3/8 = Animation appear animation

--- src/App.js
+++ src/App.js
@@ -1,90 +1,41 @@
-import { useRef, useEffect, useState } from "react";
-import "./App.css";
-
-import { gsap, Power3 } from "gsap";
+import { useRef, useEffect } from "react";
+import "./App.scss";
+import Person from "./images/person.jpg";
+import CSSRulePlugin from "gsap/CSSRulePlugin";
+import { gsap, Power2 } from "gsap";
 
 function App() {
-  let app = useRef(null);
-  let circle = useRef(null);
-  let circleRed = useRef(null);
-  let circleBlue = useRef(null);
+  let container = useRef(null);
+  let image = useRef(null);
+  let imageReveal = CSSRulePlugin.getRule(".img-container:after");
 
-  const [state, setState] = useState(false);
-
-  /* Animate circleRed onClick */
-  const handleExpand = () => {
-    gsap.to(circleRed.current, {
-      duration: 0.8,
-      width: 200,
-      height: 200,
-      ease: Power3.easeOut,
-    });
-
-    setState(true);
-  };
-
-  /* Animate circleRed onClick */
-  const handleShrink = () => {
-    gsap.to(circleRed.current, {
-      duration: 0.8,
-      width: 75,
-      height: 75,
-      ease: Power3.easeOut,
-    });
-
-    setState(false);
-  };
+  /* timeline allows animation chaining */
+  const tl = gsap.timeline();
 
   useEffect(() => {
-    /* Make app component visible */
-    gsap.to(app.current, { duration: 0, css: { visibility: "visible" } });
-
-    /* Make the circles appear with animation */
-    gsap.from([circle.current, circleRed.current, circleBlue.current], {
-      opacity: 0,
-      x: 40,
-      ease: Power3.easeOut,
-      duration: 0.8,
-      stagger: 0.2,
-    });
-
-    /* Make the circles appear with animation */
-    // gsap.from(circle.current, {
-    // duration: 0.8,
-    // opacity: 0,
-    // x: 40,
-    // ease: Power3.easeOut,
-    // });
-    // gsap.from(circleRed.current, {
-    //   duration: 0.8,
-    //   opacity: 0,
-    //   x: 40,
-    //   ease: Power3.easeOut,
-    //   delay: 0.2,
-    // });
-    // gsap.from(circleBlue.current, {
-    //   duration: 0.8,
-    //   opacity: 0,
-    //   x: 40,
-    //   ease: Power3.easeOut,
-    //   delay: 0.4,
-    // });
-  }, []);
+    tl.to(container.current, {
+      duration: 0,
+      css: { visibility: "visible" },
+    })
+      .to(imageReveal, { duration: 1.4, width: "0%", ease: Power2.easeInOut })
+      .from(image.current, {
+        duration: 1.4,
+        scale: 1.6,
+        ease: Power2.easeInOut,
+        delay: -1.6,
+      });
+  });
 
   return (
-    <div className="App" ref={app}>
-      <header className="App-header">
-        <div className="circle-container">
-          <div className="circle" ref={circle}></div>
-          <div
-            className="circle red"
-            ref={circleRed}
-            onClick={state !== true ? handleExpand : handleShrink}
-          ></div>
-          <div className="circle blue" ref={circleBlue}></div>
-        </div>
-      </header>
-    </div>
+    <section className="main">
+      <div className="container" ref={container}>
+        <>
+          <div className="img-container">
+            <img src={Person} alt="person" ref={image} />
+          </div>
+        </>
+      </div>
+    </section>
   );
 }
 
