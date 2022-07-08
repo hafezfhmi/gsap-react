cbb8b99bbf05575649bd58132eee6c47c92243a1
hafezfhmi <hafezfahmi2000@gmail.com>
2022-07-08 22:50:41 +0800

    Tutorial 5/8 = Created animation and slider functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
+import { gsap, Power3 } from "gsap";
 
 import leftArrow from "./assets/arrow-left.svg";
 import rightArrow from "./assets/arrow-right.svg";
@@ -31,10 +32,133 @@ const testimonials = [
 ];
 
 function App() {
+  let imageList = useRef(null);
+  let testimonialList = useRef(null);
+
+  const imageWidth = 340;
+
+  const [state, setState] = useState({
+    isActive1: true,
+    isActive2: false,
+    isActive3: false,
+  });
+
+  useEffect(() => {
+    gsap.to(testimonialList.current.children[0], { duration: 0, opacity: 1 });
+  }, []);
+
+  const slideLeft = (index, duration, multiplied = 1) => {
+    gsap.to(imageList.current.children[index], {
+      duration: duration,
+      x: -imageWidth * multiplied,
+      ease: Power3.easeOut,
+    });
+  };
+
+  const slideRight = (index, duration, multiplied = 1) => {
+    gsap.to(imageList.current.children[index], {
+      duration: duration,
+      x: imageWidth * multiplied,
+      ease: Power3.easeOut,
+    });
+  };
+
+  const scale = (index, duration) => {
+    gsap.from(imageList.current.children[index], {
+      duration: duration,
+      scale: 1.2,
+      ease: Power3.easeOut,
+    });
+  };
+
+  const fadeOut = (index, duration) => {
+    gsap.to(testimonialList.current.children[index], {
+      duration: duration,
+      opacity: 0,
+    });
+  };
+
+  const fadeIn = (index, duration) => {
+    gsap.to(testimonialList.current.children[index], {
+      duration: duration,
+      opacity: 1,
+      delay: 1,
+    });
+  };
+
+  const nextSlide = () => {
+    if (imageList.current.children[0].classList.contains("active")) {
+      setState({ isActive1: false, isActive2: true });
+      //Image transition
+      slideLeft(0, 1);
+      slideLeft(1, 1);
+      scale(1, 1);
+      slideLeft(2, 1);
+      slideLeft(2, 0);
+      fadeOut(0, 1);
+      fadeIn(1, 1);
+    } else if (imageList.current.children[1].classList.contains("active")) {
+      setState({ isActive2: false, isActive3: true });
+      //Image transition
+      slideRight(0, 1);
+      slideLeft(1, 1, 2);
+      slideLeft(2, 1, 2);
+      scale(2, 1);
+      //content transition
+      fadeOut(1, 1);
+      fadeIn(2, 1);
+    } else if (imageList.current.children[2].classList.contains("active")) {
+      setState({ isActive1: true, isActive3: false });
+      //Image transition
+      slideLeft(2, 1, 3);
+      slideLeft(0, 1, 0);
+      slideLeft(1, 0, 0);
+      scale(0, 1);
+      //content transition
+      fadeOut(2, 1);
+      fadeIn(0, 1);
+    }
+  };
+
+  const prevSlide = () => {
+    if (imageList.current.children[0].classList.contains("active")) {
+      setState({ isActive1: false, isActive3: true });
+      //Image transition
+      slideLeft(2, 0, 3);
+      slideLeft(2, 1, 2);
+      scale(2, 1);
+      slideRight(0, 1);
+      slideRight(1, 1);
+      //content transtion
+      fadeOut(0, 1);
+      fadeIn(2, 1);
+    } else if (imageList.current.children[1].classList.contains("active")) {
+      setState({ isActive2: false, isActive1: true });
+      //Image transition
+      slideLeft(0, 0);
+      slideRight(0, 1, 0);
+      slideRight(1, 1, 0);
+      slideRight(2, 1, 2);
+      scale(0, 1);
+      //content transtion
+      fadeOut(1, 1);
+      fadeIn(0, 1);
+    } else if (imageList.current.children[2].classList.contains("active")) {
+      setState({ isActive2: true, isActive3: false });
+      slideLeft(2, 1);
+      slideLeft(1, 0, 2);
+      slideLeft(1, 1);
+      scale(1, 1);
+      //content transtion
+      fadeOut(2, 1);
+      fadeIn(1, 1);
+    }
+  };
+
   return (
     <div className="testimonial-section">
       <div className="testimonial-container">
-        <div className="arrow left">
+        <div onClick={prevSlide} className="arrow left">
           <span>
             <img src={leftArrow} alt="left arrow" />
           </span>
@@ -42,35 +166,35 @@ function App() {
 
         <div className="inner">
           <div className="t-image">
-            <ul>
-              <li>
+            <ul ref={imageList}>
+              <li className={state.isActive1 ? "active" : ""}>
                 <img src={testimonials[0].image} alt={testimonials[0].name} />
               </li>
-              <li>
+              <li className={state.isActive2 ? "active" : ""}>
                 <img src={testimonials[1].image} alt={testimonials[1].name} />
               </li>
-              <li>
+              <li className={state.isActive3 ? "active" : ""}>
                 <img src={testimonials[2].image} alt={testimonials[2].name} />
               </li>
             </ul>
           </div>
           <div className="t-content">
-            <ul>
-              <li>
+            <ul ref={testimonialList}>
+              <li className={state.isActive1 ? "active" : ""}>
                 <div className="content-inner">
                   <p className="quote">{testimonials[0].quote}</p>
                   <h3 className="name">{testimonials[0].name}</h3>
                   <h4 className="title">{testimonials[0].title}</h4>
                 </div>
               </li>
-              <li>
+              <li className={state.isActive2 ? "active" : ""}>
                 <div className="content-inner">
                   <p className="quote">{testimonials[1].quote}</p>
                   <h3 className="name">{testimonials[1].name}</h3>
                   <h4 className="title">{testimonials[1].title}</h4>
                 </div>
               </li>
-              <li>
+              <li className={state.isActive3 ? "active" : ""}>
                 <div className="content-inner">
                   <p className="quote">{testimonials[2].quote}</p>
                   <h3 className="name">{testimonials[2].name}</h3>
@@ -81,7 +205,7 @@ function App() {
           </div>
         </div>
 
-        <div className="arrow right">
+        <div onClick={nextSlide} className="arrow right">
           <span>
             <img src={rightArrow} alt="right arrow" />
           </span>
